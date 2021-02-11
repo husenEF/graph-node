578d6d39d3a6d991a7a4edf7606af09f095a336e
Husen Efendi <husenef@gmail.com>
2021-02-12 01:49:04 +0700

    update config heroku

--- database/config/config.js
+++ database/config/config.js
@@ -38,6 +38,12 @@ const pg = {
   production: {
     url: process.env.DATABASE_URL,
     dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
   },
 };
 
