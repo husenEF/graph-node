74a9ced87b1b318bfffe437b7b2c91dd31157a2f
Husen Efendi <husenef@gmail.com>
2021-02-12 09:27:44 +0700

    update readme

--- graphql/resolvers/index.js
+++
@@ -1,5 +0,0 @@
-const userResolvers = require('./user');
-const postResolvers = require('./post');
-const commentResolvers = require('./comment');
-
-module.exports = [userResolvers, postResolvers, commentResolvers];
